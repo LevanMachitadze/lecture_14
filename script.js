9db6d4a46a1e9ff99481daa6d2cd07fe726bc6e6
Levani <machitadzelevan@gmail.com>
2024-09-09 20:57:43 +0400

    destr

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hallo world');
+const alphabet = ['A', 'B', 'C', 'D', 'E'];
+const numbers = ['1', '2', '3', '4', '5'];
+// console.log(alphabet[0]);
+// console.log(alphabet[1]);
+// console.log(alphabet[2]);
+
+// const [first, , third, ...reset] = alphabet;
+
+// console.log(first);
+// console.log(reset);
+
+const group3 = [...alphabet, ...numbers];
+
+console.log(group3);
