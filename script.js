c1b2fd532e61800648dfbde774eacd3117541b8e
Levani <machitadzelevan@gmail.com>
2024-09-09 21:15:30 +0400

    FUNCTIONS

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const alphabet = ['A', 'B', 'C', 'D', 'E'];
-const numbers = ['1', '2', '3', '4', '5'];
-// console.log(alphabet[0]);
-// console.log(alphabet[1]);
-// console.log(alphabet[2]);
+// const alphabet = ['A', 'B', 'C', 'D', 'E'];
+// const numbers = ['1', '2', '3', '4', '5'];
+// // console.log(alphabet[0]);
+// // console.log(alphabet[1]);
+// // console.log(alphabet[2]);
 
-// const [first, , third, ...reset] = alphabet;
+// // const [first, , third, ...reset] = alphabet;
 
-// console.log(first);
-// console.log(reset);
+// // console.log(first);
+// // console.log(reset);
 
-const group3 = [...alphabet, ...numbers];
+// const group3 = [...alphabet, ...numbers];
 
-console.log(group3);
+// console.log(group3);
---
+++ script2.js
@@ -0,0 +1,7 @@
+function great() {
+  console.log('Hello world');
+}
+const speak = function () {
+  console.log('Speak');
+};
+console.log(great());
